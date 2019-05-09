d06dcb37ac6eda400c75810067c23d57609bf6e7
mafof <pavel.bykov2018@mail.ru>
2019-05-09 13:43:58 +0300

    add core class in this module

--- app/Telegram/Telegram.php
+++ app/Telegram/Telegram.php
@@ -1,9 +1,46 @@
 <?php
-
+/**
+ * Главный класс работы с модулем
+ */
 
 namespace App\Telegram;
 
 
+use App\Telegram\Builders\BuilderCommand;
+use App\Telegram\Errors\ErrorCoreTelegram;
+
 class Telegram {
-    public function __construct($config = null) {}
+    private $readyCommand = null;
+    private $config = null;
+
+    /**
+     * Telegram constructor.
+     * @param string $typeSend - тип отправки, `res` - в ответ на действие пользователя, `cURL` - с помощью модуля cURL
+     */
+    public function __construct($typeSend = 'res') {
+        $this->config = new \stdClass();
+        $this->config->typeSend = $typeSend;
+    }
+
+    public function setCommand(BuilderCommand $command) {
+        $this->readyCommand = $command->buildCommand($this->config->typeSend);
+    }
+
+    public function sendCommand() {
+        if(is_null($this->readyCommand)) throw new ErrorCoreTelegram("command is not defined");
+
+        if($this->config->typeSend == 'response' || $this->config->typeSend == 'res') {
+
+            return response($this->readyCommand, 200)
+                ->header('content-type', 'application/json');
+
+        } else if($this->config->typeSend == 'curl' || $this->config->typeSend == 'cURL'){
+
+            // code...
+            return response("1", 200);
+
+        } else {
+            throw new ErrorCoreTelegram("type {$this->config->typeSend} is not defined");
+        }
+    }
 }
