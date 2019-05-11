693b52b2398ad804e407cc610702f74b7e6b2717
mafof <pavel.bykov2018@mail.ru>
2019-05-11 13:28:15 +0300

    upd routes

--- routes/webhook.php
+++ routes/webhook.php
@@ -1,3 +1,10 @@
 <?php
 
+use Illuminate\Http\Request;
+
 Route::any('/telegram/wh', 'App\Http\Controllers\Telegram@onUpdate');
+
+Route::any('/vk/wh', function(Request $req) {
+    file_put_contents(base_path("vk.txt"), json_encode($req->json()->all(), JSON_UNESCAPED_UNICODE), FILE_APPEND);
+    return response('2340d7ee', 200);
+});
