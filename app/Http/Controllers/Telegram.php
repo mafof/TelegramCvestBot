2ae43914263b117963380cf9ff0d999db3fb910e
mafof <pavel.bykov2018@mail.ru>
2019-05-11 13:27:37 +0300

    upd controller

--- app/Http/Controllers/Telegram.php
+++ app/Http/Controllers/Telegram.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Telegram\BuilderCommand;
-use App\Telegram\BuildKeyBoard;
+use App\Telegram\Builders\BuilderCommand;
+use App\Telegram\Builders\BuilderKeyBoard;
+use App\Telegram\Telegram as TelegramCore;
 
 class Telegram extends Controller
 {
@@ -14,49 +15,13 @@ class Telegram extends Controller
             $data = $req->json()->all();
             file_put_contents(base_path('logs.txt'), "===={Сообщение}====\r\n".json_encode($data, JSON_UNESCAPED_UNICODE)."\r\n", FILE_APPEND);
 
+            $data = json_decode(json_encode($data), false);
 
-            $ch = curl_init("https://api.telegram.org/bot".env("TELEGRAM_BOT_ID").":".env("TELEGRAM_BOT_TOKEN")."/getMe");
+            $tg = new TelegramCore();
+            $tg->mainHandlerCommand($data);
 
-            curl_setopt($ch, CURLOPT_POST, 1);
-
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_HEADER, true);
-
-            //curl_setopt($ch, CURLOPT_PROXY, "sss:1080");
-            //curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
-            //curl_setopt($ch, CURLOPT_PROXYUSERPWD, "ss:ss");
-
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,  2);
-
-            curl_setopt($ch,CURLOPT_TIMEOUT, 0);
-            curl_setopt($ch,CURLOPT_CONNECTTIMEOUT, 0);
-
-
-            $response = curl_exec($ch);
-
-            echo curl_error($ch);
-
-            curl_close($ch);
-
-            var_dump($response);
+            return $tg->sendCommand();
         }
-
-        $command = new BuilderCommand();
-        $command->setCommand("sendPhoto");
-        $command->appendArgument("chat_id", 322694502);
-        $command->appendArgument("caption", "Hello world 2");
-        $command->appendArgument("photo", "https://i.imgur.com/6dyEGiZ.jpg");
-
-        $keyboard = new BuildKeyBoard();
-        $keyboard->setInlineKeyBoard()
-            ->appendRow()
-            ->appendButtonInline("1 функция");
-
-        $command->setKeyboard($keyboard);
-
-        return response($command->buildCommand('res'), 200)
-            ->header('content-type', 'application/json');
 
     }
 }
