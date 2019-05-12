96d12519705ed671f22c58e078d9a10064f81f29
mafof <pavel.bykov2018@mail.ru>
2019-05-12 16:36:24 +0300

    refactoring BaseStorageActionUser

--- app/Bot/Messengers/BaseStorageActionUsers.php
+++ app/Bot/Messengers/BaseStorageActionUsers.php
@@ -1,18 +1,48 @@
 <?php
 /**
- * Интерфейс для определения методов записи/чтения последних действий пользователя
+ * Базовый класс управления временными данными пользователя
  */
 namespace App\Bot\Messengers;
 
 
-interface BaseStorageActionUsers {
-    const MAIN_MENU = 1;
-    const QUEST = 2;
-    const STATS = 3;
+class BaseStorageActionUsers {
+    const LOCATION_MAIN_MENU = 1;
+    const LOCATION_QUEST = 2;
+    const LOCATION_STATS = 3;
 
-    public function addUser($nickname);
-    public function getUser($nickname);
+    protected $PREFIX = null;
+    protected $mem = null;
 
-    public function setLocationUser($nickname, $location);
-    public function setStepQuestUser($nickname, $step);
+    public function __construct() {
+        $this->mem = new \Memcache();
+        $this->mem->connect('localhost', 11211);
+    }
+
+    public function addUser($nickname) {
+        return $this->mem->add(self::PREFIX."_{$nickname}", Array(
+            "location"        => false,
+            "stepQuest"       => false,
+            "isAcceptProfile" => false
+        ));
+    }
+
+    public function getUser($nickname) {
+        return $this->mem->get(self::PREFIX."_{$nickname}");
+    }
+
+    public function setLocationUser($nickname, $location) {
+        $userData = $this->getUser($nickname);
+        if($userData == false) return false;
+
+        $userData["location"] = $location;
+        return $this->mem->set(self::PREFIX."_{$nickname}", $userData);
+    }
+
+    public function setStepQuestUser($nickname, $step) {
+        $userData = $this->getUser($nickname);
+        if($userData == false) return false;
+
+        $userData["stepQuest"] = $step;
+        return $this->mem->set(self::PREFIX."_{$nickname}", $userData);
+    }
 }
--- app/Bot/Messengers/Telegram/TelegramStorageActionUsers.php
+++ app/Bot/Messengers/Telegram/TelegramStorageActionUsers.php
@@ -6,39 +6,6 @@ namespace App\Bot\Messengers\Telegram;
 
 use App\Bot\Messengers\BaseStorageActionUsers;
 
-class TelegramStorageActionUsers implements BaseStorageActionUsers {
-    private const PREFIX = "tg";
-    private $mem = null;
-
-    public function __construct() {
-        $this->mem = new \Memcache();
-        $this->mem->connect('localhost', 11211);
-    }
-
-    public function addUser($nickname) {
-        return $this->mem->add(self::PREFIX."_{$nickname}", Array(
-            "location"  => false,
-            "stepQuest" => false
-        ));
-    }
-
-    public function getUser($nickname) {
-        return $this->mem->get(self::PREFIX."_{$nickname}");
-    }
-
-    public function setLocationUser($nickname, $location) {
-        $userData = $this->getUser($nickname);
-        if($userData == false) return false;
-
-        $userData["location"] = $location;
-        return $this->mem->set(self::PREFIX."_{$nickname}", $userData);
-    }
-
-    public function setStepQuestUser($nickname, $step) {
-        $userData = $this->getUser($nickname);
-        if($userData == false) return false;
-
-        $userData["stepQuest"] = $step;
-        return $this->mem->set(self::PREFIX."_{$nickname}", $userData);
-    }
+class TelegramStorageActionUsers extends BaseStorageActionUsers {
+    protected $PREFIX = "tg";
 }
