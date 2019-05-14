36fbf3b202dc4994bcf14e83150a85f870941940
mafof <pavel.bykov2018@mail.ru>
2019-05-14 19:14:12 +0300

    remove builder and added logic then models

--- database/seeds/GroupsPrivilegesSeed.php
+++ database/seeds/GroupsPrivilegesSeed.php
@@ -8,76 +8,56 @@ class GroupsPrivilegesSeed extends Seeder {
     public function run() {
         // Группа пользователь =>
         DB::table('groups_privileges')->insert([
-            'group_id' =>
-                DB::table('groups')->select('id')->where('name', 'Пользователь')->get()[0]->id,
-            'privilege_id' =>
-                DB::table('privileges')->select('id')->where('name', 'Управление аккаунтом')->get()[0]->id
+            'group_id' => App\Group::where('name', 'Пользователь')->first()->id,
+            'privilege_id' => App\Privilege::where('name', 'Управление аккаунтом')->first()->id
         ]);
 
         // Группа писатель =>
         DB::table('groups_privileges')->insert([
-            'group_id' =>
-                DB::table('groups')->select('id')->where('name', 'Писатель')->get()[0]->id,
-            'privilege_id' =>
-                DB::table('privileges')->select('id')->where('name', 'Управление аккаунтом')->get()[0]->id
+            'group_id' => App\Group::where('name', 'Писатель')->first()->id,
+            'privilege_id' => App\Privilege::where('name', 'Управление аккаунтом')->first()->id
         ]);
 
         DB::table('groups_privileges')->insert([
-            'group_id' =>
-                DB::table('groups')->select('id')->where('name', 'Писатель')->get()[0]->id,
-            'privilege_id' =>
-                DB::table('privileges')->select('id')->where('name', 'Создание квеста')->get()[0]->id
+            'group_id' => App\Group::where('name', 'Писатель')->first()->id,
+            'privilege_id' => App\Privilege::where('name', 'Создание квеста')->first()->id
         ]);
 
         // Группа Модератор =>
         DB::table('groups_privileges')->insert([
-            'group_id' =>
-                DB::table('groups')->select('id')->where('name', 'Модератор')->get()[0]->id,
-            'privilege_id' =>
-                DB::table('privileges')->select('id')->where('name', 'Управление аккаунтом')->get()[0]->id
+            'group_id' => App\Group::where('name', 'Модератор')->first()->id,
+            'privilege_id' => App\Privilege::where('name', 'Управление аккаунтом')->first()->id
         ]);
 
         DB::table('groups_privileges')->insert([
-            'group_id' =>
-                DB::table('groups')->select('id')->where('name', 'Модератор')->get()[0]->id,
-            'privilege_id' =>
-                DB::table('privileges')->select('id')->where('name', 'Создание квеста')->get()[0]->id
+            'group_id' => App\Group::where('name', 'Модератор')->first()->id,
+            'privilege_id' => App\Privilege::where('name', 'Создание квеста')->first()->id
         ]);
 
         DB::table('groups_privileges')->insert([
-            'group_id' =>
-                DB::table('groups')->select('id')->where('name', 'Модератор')->get()[0]->id,
-            'privilege_id' =>
-                DB::table('privileges')->select('id')->where('name', 'Управление квестами')->get()[0]->id
+            'group_id' => App\Group::where('name', 'Модератор')->first()->id,
+            'privilege_id' => App\Privilege::where('name', 'Управление квестами')->first()->id
         ]);
 
         // Группа Администратор =>
         DB::table('groups_privileges')->insert([
-            'group_id' =>
-                DB::table('groups')->select('id')->where('name', 'Администратор')->get()[0]->id,
-            'privilege_id' =>
-                DB::table('privileges')->select('id')->where('name', 'Управление аккаунтом')->get()[0]->id
+            'group_id' => App\Group::where('name', 'Администратор')->first()->id,
+            'privilege_id' => App\Privilege::where('name', 'Управление аккаунтом')->first()->id
         ]);
 
         DB::table('groups_privileges')->insert([
-            'group_id' =>
-                DB::table('groups')->select('id')->where('name', 'Администратор')->get()[0]->id,
-            'privilege_id' =>
-                DB::table('privileges')->select('id')->where('name', 'Создание квеста')->get()[0]->id
+            'group_id' => App\Group::where('name', 'Администратор')->first()->id,
+            'privilege_id' => App\Privilege::where('name', 'Создание квеста')->first()->id
         ]);
 
         DB::table('groups_privileges')->insert([
-            'group_id' =>
-                DB::table('groups')->select('id')->where('name', 'Администратор')->get()[0]->id,
-            'privilege_id' =>
-                DB::table('privileges')->select('id')->where('name', 'Управление квестами')->get()[0]->id
+            'group_id' => App\Group::where('name', 'Администратор')->first()->id,
+            'privilege_id' => App\Privilege::where('name', 'Управление квестами')->first()->id
         ]);
 
         DB::table('groups_privileges')->insert([
-            'group_id' =>
-                DB::table('groups')->select('id')->where('name', 'Администратор')->get()[0]->id,
-            'privilege_id' =>
-                DB::table('privileges')->select('id')->where('name', 'Управление аккаунтами')->get()[0]->id
+            'group_id' => App\Group::where('name', 'Администратор')->first()->id,
+            'privilege_id' => App\Privilege::where('name', 'Управление аккаунтами')->first()->id
         ]);
     }
 }
