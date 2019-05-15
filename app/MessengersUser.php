4b13cba4c63f4346280b76566d2ac05912967bbb
mafof <pavel.bykov2018@mail.ru>
2019-05-16 01:06:45 +0300

    update model

--- app/MessengersUser.php
+++ app/MessengersUser.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class MessengersUser extends Model {
     protected $table = "messengers_users";
     public $timestamps = false;
+
+    protected $fillable = [
+        'messenger_identifier', 'quest_done_count', 'quest_created_count', 'messenger_type', 'user_id'
+    ];
+
+    public static function getUser($identifier) {
+        return MessengersUser::where('messenger_identifier', $identifier)->get()->first();
+    }
 }
